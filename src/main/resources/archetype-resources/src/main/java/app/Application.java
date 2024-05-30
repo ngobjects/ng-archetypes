a154b6c15b1894a095c88ee156d0b687569413d6
Hugi Thordarson <hugi@karlmenn.is>
2024-05-30 15:42:47 +0000

    Initial import

---
+++ src/main/resources/archetype-resources/src/main/java/app/Application.java
@@ -0,0 +1,10 @@
+package ${package}.app;
+
+import ng.appserver.NGApplication;
+
+public class Application extends NGApplication {
+
+	public static void main( String[] args ) {
+		NGApplication.run( args, Application.class );
+	}
+}
